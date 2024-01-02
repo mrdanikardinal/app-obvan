1774503bca9a4721f8aa6e11960480d5780f2069
mrdanikardinal <danikardinal75@gmail.com>
2024-01-02 09:06:15 +0700

    modify side_bar in office true

--- app/Views/layout/side_bar.php
+++ app/Views/layout/side_bar.php
@@ -2,45 +2,22 @@
     <nav class="sb-sidenav accordion sb-sidenav-light" id="sidenavAccordion">
         <div class="sb-sidenav-menu">
             <div class="nav">
-                <div class="sb-sidenav-menu-heading">Core</div>
+                <div class="sb-sidenav-menu-heading">HOME</div>
                 <a class="nav-link" href="/">
                     <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                    Dashboard
+                    DASHBOARD
                 </a>
-                <div class="sb-sidenav-menu-heading">Menu</div>
-                <!-- <a class="nav-link collapsed" href="<?=base_url()?>/inventaris" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts"> -->
-                <a class="nav-link collapsed" href="<?=base_url()?>inventaris">
-                    <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                    Inventaris
-                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                </a>
-                <!-- <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                    <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="<?=base_url()?>inventaris">
-                            <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                            Gudang
-                        </a>
-                        <a class="nav-link" href="inventaris_sng_van">
-                            <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                            SNG-VAN
-                        </a>
-                        <a class="nav-link" href="inventaris_kamera">
-                            <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                            Kamera
-                        </a>
-                    </nav>
-                </div> -->
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+                <div class="sb-sidenav-menu-heading">Keuangan</div>
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePagesKasObvan" aria-expanded="false" aria-controls="collapsePages">
                     <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                    List OB Crew
+                    KAS-OBVAN
                     <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                 </a>
-                <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
+                <div class="collapse" id="collapsePagesKasObvan" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
                         <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
-                            <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                            Dalam Kota
-
+                           
+                            Kas-Rutin
                         </a>
                         <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
                             <nav class="sb-sidenav-menu-nested nav">
@@ -49,9 +26,8 @@
                                 <a class="nav-link" href="password.html">Forgot Password</a>
                             </nav>
                         </div>
-                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
-                            <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                            Luar Kota
+                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError"> 
+                            Kas-OB
                         </a>
                         <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
                             <nav class="sb-sidenav-menu-nested nav">
@@ -62,13 +38,38 @@
                         </div>
                     </nav>
                 </div>
-                
-                <a class="nav-link" href="<?=base_url()?>peminjaman-alat">
+                <div class="sb-sidenav-menu-heading">SERVICES</div>
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePagesPeralatanKeluar" aria-expanded="false" aria-controls="collapsePages">
                     <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                    Peminjaman Alat
+                    PERALATAN KELUAR
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                 </a>
-
-
+                <div class="collapse" id="collapsePagesPeralatanKeluar" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
+                        <a class="nav-link" href="<?= base_url() ?>peminjaman-alat">
+                            Crew-OB
+                        </a>
+                        <a class="nav-link" href="<?= base_url() ?>peminjaman-alat">
+                            Peminjaman-Alat
+                        </a>
+                    </nav>
+                </div>
+                <div class="sb-sidenav-menu-heading">MASTER DATA</div>
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePagesMasterData" aria-expanded="false" aria-controls="collapsePages">
+                    <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                    DATABASE
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="collapsePagesMasterData" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
+                        <a class="nav-link collapsed" href="<?= base_url() ?>inventaris">
+                            Data-Inventaris
+                        </a>
+                        <a class="nav-link collapsed" href="<?= base_url() ?>inventaris">
+                            Data-Status
+                        </a>
+                    </nav>
+                </div>
                 <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages2" aria-expanded="false" aria-controls="collapsePages2">
                     <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                     Coming Soon
@@ -100,15 +101,6 @@
                         </div>
                     </nav>
                 </div>
-                <div class="sb-sidenav-menu-heading">Addons</div>
-                <a class="nav-link" href="charts.html">
-                    <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
-                    Charts
-                </a>
-                <a class="nav-link" href="tables.html">
-                    <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                    Tables
-                </a>
             </div>
         </div>
         <div class="sb-sidenav-footer">
