e0d7a63a7aec23741292aa17a7a4b91a77caa033
mrdanikardinal <danikardinal75@gmail.com>
2024-03-18 20:22:09 +0700

    perbaikan di outbroadcast CRUD dan Peminjaman CRUD

--- app/Controllers/PeminjamanAlat.php
+++ app/Controllers/PeminjamanAlat.php
@@ -45,6 +45,7 @@ class PeminjamanAlat extends BaseController
     public function create()
     {
 
+        // dd($dataInv = $this->pinjamAlatModel->procedureGetItemsReady());
         $dataInv = $this->pinjamAlatModel->procedureGetItemsReady();
         $generator = new BarcodeGeneratorPNG();
         // dd($dataInv);
