6a04be2e53bbbed223e83888699741e753f95ed6
mrdanikardinal <danikardinal75@gmail.com>
2024-03-25 00:25:19 +0700

    perbaikan di fitur surat tugas print out

--- app/Views/user/out_broadcast.php
+++ app/Views/user/out_broadcast.php
@@ -12,9 +12,10 @@
         th,
         td {
             width: 100%;
+
             /* Set lebar tabel menjadi 50% dari lebar halaman */
             border-collapse: collapse;
-            table-layout: auto;
+            /* table-layout: auto; */
             /* Set lebar kolom mengikuti konten */
         }
 
@@ -23,6 +24,8 @@
             border: 1px solid black;
             padding: 10px;
             text-align: left;
+            /* height: 20px;
+            line-height:10px; */
         }
 
         span {
@@ -99,10 +102,11 @@
     </nav>
     <section>
         <table>
-            <table style="width:100%">
+            <!-- <table cellpadding="5"> -->
+            <table cellpadding="5" style="border-collapse: collapse;">
                 <thead>
                     <tr>
-                        <th style="width:8%">NO</th>
+                        <th style="width:7%">NO</th>
                         <th>NAMA</th>
                         <th>NIP</th>
                         <th>GOLONGAN</th>
@@ -110,13 +114,12 @@
                     </tr>
                 </thead>
                 <tbody>
-
                     <?php $number = 1; ?>
                     <?php foreach ($showAllJoinsOBKategori as $key => $valueShowBroadcast) : ?>
                         <?php foreach ($allDataOutBroadcast as $j) : ?>
                             <?php if ($valueShowBroadcast['id_ob'] == $j['id_ob']) : ?>
                                 <tr>
-                                    <th class="text-center" style="width:8%"><?= $number++; ?></th>
+                                    <th class="text-center" style="width:7%"><?= $number++; ?></th>
                                     <td class="text-center">
                                         <?php foreach ($allUsers as $k) : ?>
                                             <?php if ($j['id_users'] == $k['id']) : ?>
@@ -124,18 +127,35 @@
                                             <?php endif; ?>
                                         <?php endforeach; ?>
                                     </td>
-                                    <td><?= $k['nip']; ?></td>
-                                    <td><?= $k['golongan']; ?></td>
-                                    <td><?= $k['npwp']; ?></td>
+                                    <td>
+                                        <?php foreach ($allUsers as $k) : ?>
+                                            <?php if ($j['id_users'] == $k['id']) : ?>
+                                                <?= $k['nip']; ?>
+                                            <?php endif; ?>
+                                        <?php endforeach; ?>
+
+                                    </td>
+                                    <td>
+                                        <?php foreach ($allUsers as $k) : ?>
+                                            <?php if ($j['id_users'] == $k['id']) : ?>
+                                                <?= $k['golongan']; ?>
+                                            <?php endif; ?>
+                                        <?php endforeach; ?>
+
+                                    </td>
+                                    <td>
+                                        <?php foreach ($allUsers as $k) : ?>
+                                            <?php if ($j['id_users'] == $k['id']) : ?>
+                                                <?= $k['npwp']; ?>
+                                            <?php endif; ?>
+                                        <?php endforeach; ?>
+
+                                    </td>
 
                                 </tr>
                             <?php endif; ?>
                         <?php endforeach; ?>
-
                     <?php endforeach; ?>
-
-
-
                 </tbody>
                 <!-- <tfoot>
                     <tr>
@@ -151,25 +171,57 @@
 
     ?>
     <footer>
-        <h5>Jakarta,
+        <h5 style="text-indent: 200px;">Jakarta,
             <?php if ($valueShowBroadcast['tanggal'] != $valueShowBroadcast['sampai_dengan']) : ?>
-                <?= $tanggal-2; ?>
+                <?= $tanggal - 2; ?>
                 <?= $bulan_indonesia; ?>
                 <?= $tahun; ?>
             <?php else : ?>
-                <?= $tanggal-2; ?>
+                <?= $tanggal - 2; ?>
                 <?= $bulan_indonesia; ?>
                 <?= $tahun; ?>
             <?php endif; ?>
 
         </h5>
-        <h5>KETUA TIM TEKNOLOGI PERALATAN LUAR STUDIO</h5>
 
+        <!-- <table cellspacing="0" cellpadding="0" style="text-align: right ; width: 50%;">
+            <thead>
+                <tr>
+                    <td markdown="span" style="padding: 0.5vw; border: none; max-width:100%; max-height:10%; background: transparent;">
+                        <h5>KETUA TIM TEKNOLOGI PERALATAN LUAR STUDIO</h5>
+                    </td>
+                </tr>
+                <tr>
+                    <td markdown="span" style="padding: 0.5vw; border: none; max-width:100%; max-height:10%; background: transparent;">
+                        <img src="img/tvri.png" alt="logo-tvri" width="100px" height="50px" style="text-align: right">
+                    </td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td markdown="span" style="padding: 0.5vw; border: none; max-width:100%; max-height:10%; background: transparent;">
+                        <h5>JANSEN STEPPEN JOU SINAGA</h5>
 
+                    </td>
+                </tr>
 
+                <tr>
+                    <td markdown="span" style="padding: 0.5vw; border: none; max-width:100%; max-height:10%; background: transparent;">
+                        <h5>198809232022211006</h5>
+                    </td>
+                </tr>
 
-        <h5>JANSEN STEPPEN JOU SINAGA</h5>
-        <h5>198809232022211006</h5>
+            </tbody>
+        </table> -->
+        <h5 style="text-indent: 200px;">KETUA TIM TEKNOLOGI PERALATAN LUAR STUDIO</h5>
+        <img src="img/tvri.png" alt="logo-tvri" width="100px" height="50px" style="text-align: center;">
+        <h5 style="text-indent: 200px;">JANSEN STEPPEN JOU SINAGA</h5>
+        <h5 style="text-indent: 200px;">198809232022211006</h5>
+        <!-- <h5 style="text-align: right">KETUA TIM TEKNOLOGI PERALATAN LUAR STUDIO</h5>
+        <img src="img/tvri.png" alt="logo-tvri" width="100px" height="50px" style="text-align: right">
+
+        <h5 style="text-align: right">JANSEN STEPPEN JOU SINAGA</h5>
+        <h5 style="text-align: right">198809232022211006</h5> -->
 
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
